645f9db7f192214a0be435e882e093e90de5185d
Austin Sands <austinrsands@gmail.com>
2021-04-21 19:55:31 -0500

    added exploding block

---
+++ Assets/Scripts/Blocks/ExplodingBlockController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ExplodingBlockController : BlockController
+{
+
+    [SerializeField]
+    private float force = 50f;
+
+    private new Rigidbody2D rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Block"))
+            collision.collider.GetComponent<Rigidbody2D>().AddForce(-collision.relativeVelocity.normalized * force);
+    }
+}
